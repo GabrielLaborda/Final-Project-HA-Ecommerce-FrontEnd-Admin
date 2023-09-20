69d0fc5ab1106ba99dd52eb759cb7ee65f6daa5b
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-20 12:17:34 -0300

    Va con cambios

--- src/components/AddProductModal.jsx
+++ src/components/AddProductModal.jsx
@@ -4,7 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import { useSelector } from 'react-redux';
 
-function AddProductModal() {
+function AddProductModal({ getAllProducts, setAllProducts }) {
   const baseURL = import.meta.env.VITE_API_BASE_URL;
   const [show, setShow] = useState(false);
   const loggedAdmin = useSelector((state) => state.admin);
@@ -50,31 +50,15 @@ function AddProductModal() {
     setPicture(pictures);
   };
 
-  const handleSubmit = async () => {
-    console.log(name);
-    console.log(description);
-    console.log(picture);
-    console.log(price);
-    console.log(stock);
-    console.log(featured);
-    console.log(category);
-
+  const handleSubmit = async (event) => {
     try {
-      const formData = new FormData();
-
-      formData.append('name', name);
-      formData.append('description', description);
-      formData.append('picture', picture);
-      formData.append('price', price);
-      formData.append('stock', stock);
-      formData.append('featured', featured);
-      formData.append('category', category);
-
+      const formData = new FormData(event.target);
       await axios({
         method: 'POST',
         url: `${baseURL}/products`,
         data: formData,
         headers: {
+          'Content-Type': 'multipart/form-data',
           Authorization: `Bearer ${loggedAdmin.token}`,
         },
       });
@@ -86,6 +70,8 @@ function AddProductModal() {
       setPrice(0);
       setStock(0);
       setFeatured(false);
+
+      getAllProducts();
     } catch (error) {
       console.log(error);
       window.alert('Error: ' + error);
@@ -107,9 +93,10 @@ function AddProductModal() {
             </Modal.Header>
             <Modal.Body>
               <Form validated={validated} onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Group className="mb-3" controlId="name">
                   <Form.Label className="fw-bold">New product name</Form.Label>
                   <Form.Control
+                    name="name"
                     onChange={(e) => setName(e.target.value)}
                     value={name}
                     type="text"
@@ -117,44 +104,49 @@ function AddProductModal() {
                     autoFocus
                   />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Group className="mb-3" controlId="description">
                   <Form.Label className="fw-bold">Description</Form.Label>
                   <Form.Control
+                    name="description"
                     onChange={(e) => setDescription(e.target.value)}
                     value={description}
                     type="text"
                     placeholder="product description"
                   />
                 </Form.Group>
-                <Form.Group controlId="formFileMultiple" className="mb-3">
+                <Form.Group controlId="picture" className="mb-3">
                   <Form.Label className="fw-bold">Pictures</Form.Label>
                   <Form.Control
+                    name="picture"
                     onChange={(e) => hanldeFilesPictures(e.target.files)}
                     type="file"
                     multiple
                   />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Group className="mb-3" controlId="price">
                   <Form.Label className="fw-bold">Price</Form.Label>
                   <Form.Control
+                    name="price"
                     onChange={(e) => setPrice(e.target.value)}
                     value={price}
                     type="number"
                     placeholder="unit price"
                   />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Group className="mb-3" controlId="stock">
                   <Form.Label className="fw-bold">Initial stock</Form.Label>
                   <Form.Control
+                    name="stock"
                     onChange={(e) => setStock(e.target.value)}
                     value={stock}
                     type="number"
                     placeholder="quantity"
                   />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Group className="mb-3" controlId="category">
                   <Form.Label className="fw-bold">Category</Form.Label>
                   <Form.Select
+                    name="category"
                     aria-label="Status"
                     value={category}
                     onChange={(e) => {
@@ -169,33 +161,28 @@ function AddProductModal() {
                     ))}
                   </Form.Select>
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Group className="mb-3" controlId="featured">
                   <Form.Label className="fw-bold">Featured</Form.Label>
                   <Form.Check
+                    name="featured"
                     onChange={(e) => setFeatured(!featured)}
                     value={featured}
                     type="switch"
-                    id="featured"
                     label="Featured"
                   />
                 </Form.Group>
+                <hr />
+                <button type="submit" className="ms-3 px-3 py-1 btn btn-dark rounded-0">
+                  Save new product
+                </button>
+                <button
+                  onClick={handleClose}
+                  className="ms-3 px-3 py-1 btn btn-outline-dark rounded-0"
+                >
+                  Close
+                </button>
               </Form>
             </Modal.Body>
-            <Modal.Footer>
-              <button
-                onClick={handleClose}
-                className="ms-3 px-3 py-1 btn btn-outline-dark rounded-0"
-              >
-                Close
-              </button>
-              <button
-                type="submit"
-                onClick={handleSubmit}
-                className="ms-3 px-3 py-1 btn btn-dark rounded-0"
-              >
-                Save new product
-              </button>
-            </Modal.Footer>
           </Modal>
         </>
       )}
