210bd6ad48c1d2e5f32927718e6a48ec9c1f1543
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-20 10:50:53 -0300

    Subo los ajustes de add product modal

--- src/components/AddProductModal.jsx
+++ src/components/AddProductModal.jsx
@@ -5,144 +5,197 @@ import Modal from 'react-bootstrap/Modal';
 import { useSelector } from 'react-redux';
 
 function AddProductModal() {
+  const baseURL = import.meta.env.VITE_API_BASE_URL;
+  const [show, setShow] = useState(false);
+  const loggedAdmin = useSelector((state) => state.admin);
+  const [validated, setValidated] = useState(false);
 
-    const baseURL = import.meta.env.VITE_API_BASE_URL;
-    const [show, setShow] = useState(false);
-    const loggedAdmin = useSelector((state) => state.admin);
-    const [validated, setValidated] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  // get category options (form options)
 
-    // get category options (form options)
+  const [allCategories, setAllCategories] = useState([]);
 
-    const [allCategories, setAllCategories] = useState([]);
-
-        const getAllCategories = async () => {
-        try {
-            const response = await axios({
-            method: 'GET',
-            url: `${baseURL}/categories`,
-            headers: {
-            Authorization: `Bearer ${loggedAdmin.token}`,
-            },
-            });
-            setAllCategories(response.data);
-        } catch (error) {
-            console.log(error);
-        }
+  const getAllCategories = async () => {
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: `${baseURL}/categories`,
+        headers: {
+          Authorization: `Bearer ${loggedAdmin.token}`,
+        },
+      });
+      setAllCategories(response.data);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
+  useEffect(() => {
     getAllCategories();
-    }, []);
+  }, []);
 
-    // post new product data
+  // post new product data
 
-    const [name, setName] = useState("");
-    const [description, setDescription] = useState("");
-    const [picture, setPicture] = useState(null);
-    const [price, setPrice] = useState(0);
-    const [stock, setStock] = useState(0);
-    const [featured, setFeatured] = useState(false);
-    const [category, setCategory] = useState("");
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [picture, setPicture] = useState(null);
+  const [price, setPrice] = useState(0);
+  const [stock, setStock] = useState(0);
+  const [featured, setFeatured] = useState(false);
+  const [category, setCategory] = useState('');
 
-    const handleSubmit = async () => {
-        try {
-            const response = await axios({
-                method: "POST",
-                url: `${baseURL}/products`,
-                data: { name, description, picture, price, stock, featured, category},
-                headers: {
-                    Authorization: `Bearer ${loggedAdmin.token}`,
-                },
-            })
-            setValidated(true);
-            handleClose();
-            setName("");
-            setDescription("");
-            setPicture([]);
-            setPrice(0);
-            setStock(0);
-            setFeatured(false);
-        } catch (error) {
-            console.log(error);
-            window.alert("Error: " + error);
-            // add Toast!!
-        }
+  const hanldeFilesPictures = (pictures) => {
+    setPicture(pictures);
+  };
+
+  const handleSubmit = async () => {
+    console.log(name);
+    console.log(description);
+    console.log(picture);
+    console.log(price);
+    console.log(stock);
+    console.log(featured);
+    console.log(category);
+    try {
+      const response = await axios({
+        method: 'POST',
+        url: `${baseURL}/products`,
+        data: {
+          name: name,
+          description: description,
+          picture: picture,
+          price: price,
+          stock: stock,
+          featured: featured,
+          category: category,
+        },
+        headers: {
+          Authorization: `Bearer ${loggedAdmin.token}`,
+        },
+      });
+      setValidated(true);
+      handleClose();
+      setName('');
+      setDescription('');
+      setPicture([]);
+      setPrice(0);
+      setStock(0);
+      setFeatured(false);
+    } catch (error) {
+      console.log(error);
+      window.alert('Error: ' + error);
+      // add Toast!!
     }
+  };
 
-
-    return (
+  return (
+    <>
+      {allCategories && (
         <>
-            {allCategories && (<>
-        <button onClick={handleShow} className='ms-3 px-5 py-2 btn btn-outline-dark rounded-0'>Add new product</button>
+          <button onClick={handleShow} className="ms-3 px-5 py-2 btn btn-outline-dark rounded-0">
+            Add new product
+          </button>
 
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Add new product</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-                <Form validated={validated} onSubmit={handleSubmit}>
-                    <Form.Group
-                        className="mb-3"
-                        controlId="exampleForm.ControlTextarea1">
-                        <Form.Label className='fw-bold'>New product name</Form.Label>
-                        <Form.Control onChange={(e) => setName(e.target.value)} value={name} type='text' placeholder='product name' autoFocus/>
-                    </Form.Group>
-                    <Form.Group
-                        className="mb-3"
-                        controlId="exampleForm.ControlTextarea1">
-                        <Form.Label className='fw-bold'>Description</Form.Label>
-                        <Form.Control onChange={(e) => setDescription(e.target.value)} value={description} type='text' placeholder='product description'/>
-                    </Form.Group>
-                    <Form.Group controlId="formFileMultiple" className="mb-3">
-                        <Form.Label className='fw-bold'>Pictures</Form.Label>
-                        <Form.Control onChange={(e) => setPicture(e.target.value.files)} type="file" multiple />
-                        </Form.Group>
-                    <Form.Group
-                        className="mb-3"
-                        controlId="exampleForm.ControlTextarea1">
-                        <Form.Label className='fw-bold'>Price</Form.Label>
-                        <Form.Control onChange={(e) => setPrice(e.target.value)} value={price} type='number' placeholder='unit price'/>
-                    </Form.Group>
-                    <Form.Group
-                        className="mb-3"
-                        controlId="exampleForm.ControlTextarea1">
-                        <Form.Label className='fw-bold'>Initial stock</Form.Label>
-                        <Form.Control onChange={(e) => setStock(e.target.value)} value={stock} type='number' placeholder='quantity'/>
-                    </Form.Group>
-                    <Form.Group
-                        className="mb-3"
-                        controlId="exampleForm.ControlTextarea1">
-                        <Form.Label className='fw-bold'>Category</Form.Label>
-                            <Form.Select aria-label="Status" value={category} onChange={(e) => {setCategory(e.currentTarget.value)
-                            }}>
-                                <option>Select product category</option>
-                                {allCategories.map((category) => (
-                                <option value={category._id} key={category._id}>{category.name}
-                                </option>
-                                ))}
-                            </Form.Select>
-                    </Form.Group>
-                    <Form.Group
-                        className="mb-3"
-                        controlId="exampleForm.ControlTextarea1">
-                        <Form.Label className='fw-bold'>Featured</Form.Label>
-                            <Form.Check
-                                onChange={(e) => setFeatured(!featured)} value={featured}
-                            type="switch"
-                            id="featured"
-                                label="Featured" />
-                    </Form.Group>
-                </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <button onClick={handleClose} className='ms-3 px-3 py-1 btn btn-outline-dark rounded-0'>Close</button>
-                    <button type='submit' onClick={handleSubmit} className='ms-3 px-3 py-1 btn btn-dark rounded-0'>Save new product</button>
-                </Modal.Footer>
-      </Modal>
-            </>)}
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Add new product</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form validated={validated} onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label className="fw-bold">New product name</Form.Label>
+                  <Form.Control
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                    type="text"
+                    placeholder="product name"
+                    autoFocus
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label className="fw-bold">Description</Form.Label>
+                  <Form.Control
+                    onChange={(e) => setDescription(e.target.value)}
+                    value={description}
+                    type="text"
+                    placeholder="product description"
+                  />
+                </Form.Group>
+                <Form.Group controlId="formFileMultiple" className="mb-3">
+                  <Form.Label className="fw-bold">Pictures</Form.Label>
+                  <Form.Control
+                    onChange={(e) => hanldeFilesPictures(e.target.files)}
+                    type="file"
+                    multiple
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label className="fw-bold">Price</Form.Label>
+                  <Form.Control
+                    onChange={(e) => setPrice(e.target.value)}
+                    value={price}
+                    type="number"
+                    placeholder="unit price"
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label className="fw-bold">Initial stock</Form.Label>
+                  <Form.Control
+                    onChange={(e) => setStock(e.target.value)}
+                    value={stock}
+                    type="number"
+                    placeholder="quantity"
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label className="fw-bold">Category</Form.Label>
+                  <Form.Select
+                    aria-label="Status"
+                    value={category}
+                    onChange={(e) => {
+                      setCategory(e.currentTarget.value);
+                    }}
+                  >
+                    <option>Select product category</option>
+                    {allCategories.map((category) => (
+                      <option value={category._id} key={category._id}>
+                        {category.name}
+                      </option>
+                    ))}
+                  </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label className="fw-bold">Featured</Form.Label>
+                  <Form.Check
+                    onChange={(e) => setFeatured(!featured)}
+                    value={featured}
+                    type="switch"
+                    id="featured"
+                    label="Featured"
+                  />
+                </Form.Group>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <button
+                onClick={handleClose}
+                className="ms-3 px-3 py-1 btn btn-outline-dark rounded-0"
+              >
+                Close
+              </button>
+              <button
+                type="submit"
+                onClick={handleSubmit}
+                className="ms-3 px-3 py-1 btn btn-dark rounded-0"
+              >
+                Save new product
+              </button>
+            </Modal.Footer>
+          </Modal>
+        </>
+      )}
     </>
   );
 }
--- src/components/AdminHome.jsx
+++ src/components/AdminHome.jsx
@@ -1,33 +1,36 @@
-import React from "react";
-import DashboardMenu from "./DashboardMenu";
-import "./AdminHome.css"
-import { Outlet, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import AdminLogin from "./AdminLogin";
-import { useEffect } from "react";
+import React from 'react';
+import DashboardMenu from './DashboardMenu';
+import './AdminHome.css';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import AdminLogin from './AdminLogin';
+import { useEffect } from 'react';
 
 function AdminHome() {
+  const loggedAdmin = useSelector((state) => state.admin);
 
-    const loggedAdmin = useSelector((state) => state.admin);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  useEffect(() => {
+    if (!loggedAdmin) {
+      return navigate('/login');
+    }
+  }, []);
 
-    useEffect(() => { if (!loggedAdmin) return navigate("/login"); }), [];
-
-    return (
-        <>
-            <div className="container-fluid" >
-                <div className="row g-0">
-                    <div className="col-2">
-                        <DashboardMenu />
-                    </div>
-                    <div className="col d-flex justify-content-center">
-                        <Outlet />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row g-0">
+          <div className="col-2">
+            <DashboardMenu />
+          </div>
+          <div className="col d-flex justify-content-center">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default AdminHome;
