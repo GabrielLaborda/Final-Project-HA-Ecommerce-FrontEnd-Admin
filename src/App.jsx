e09c13e8a44cbb2681d6a83d01c3f3ca9ef58691
soloSouza <134002966+soloSouza@users.noreply.github.com>
2023-09-27 10:14:02 -0300

    Agregamos el noMatch a las rutas de admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import AdminHome from './components/AdminHome';
 import DashboardMenu from './components/DashboardMenu';
 import HomeMainSection from './components/HomeMainSection';
@@ -10,20 +10,22 @@ import OrdersMainSection from './components/OrdersMainSection';
 import StaffMainSection from './components/StaffMainSection';
 import AdminLogin from './components/AdminLogin';
 import { ToastContainer, toast } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
+import NoMatch from './components/NoMatch';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/login' element={<AdminLogin />} />
-        <Route path='/' element={<AdminHome />}>
+        <Route path="/login" element={<AdminLogin />} />
+        <Route path="/" element={<AdminHome />}>
           <Route path="/" element={<HomeMainSection />} />
           <Route path="/users" element={<UsersMainSection />} />
           <Route path="/categories" element={<CategoriesMainSection />} />
           <Route path="/products" element={<ProductsMainSection />} />
           <Route path="/orders" element={<OrdersMainSection />} />
           <Route path="/staff" element={<StaffMainSection />} />
+          <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
       <ToastContainer />
